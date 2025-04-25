e26a50d12094315f08be866ab2e7b63610d3773b
David Schmitz <david.schmitz.privat@gmail.com>
2025-04-25 16:58:54 +0200

    Allow seed sa admin on platform

--- platform.tf
+++ platform.tf
@@ -1,4 +1,12 @@
+resource "google_project_iam_member" "project_iam_member" {
+  project = var.platform_project_id
+  role    = "roles/iam.serviceAccountAdmin"
+  member  = "serviceAccount:${var.seed_sa_email_address}"
+}
+
 module "platform" {
+  depends_on = [google_project_iam_member.project_iam_member]
+
   source     = "./platform"
   project_id = var.platform_project_id
 }
--- variables.tf
+++ variables.tf
@@ -7,3 +7,9 @@ variable "platform_project_id" {
   type        = string
   description = "Id of the project where the platform resources will be deployed"
 }
+
+variable "seed_sa_email_address" {
+  type        = string
+  sensitive   = true
+  description = "Email address of the seed service account"
+}
