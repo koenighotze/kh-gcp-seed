4c5fc5c7c826c98238816300158063ff3fd17d19
David Schmitz <david.schmitz.privat@gmail.com>
2025-04-29 17:40:25 +0200

    Output resource values

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,10 @@
+output "platform_admin_service_account_email" {
+  description = "The email of the service account"
+  value       = module.platform.admin_service_account.email
+}
+
+output "platform_terraform_state_bucket_name" {
+  value       = module.platform.terraform_state_bucket_name
+  description = "The name of the GCS bucket used for Terraform state"
+}
 
--- platform/output.tf
+++ platform/output.tf
@@ -3,10 +3,10 @@ output "admin_service_account" {
   value       = google_service_account.admin
 }
 
-# output "terraform_state_bucket_name" {
-#   value       = google_storage_bucket.terraform_state.name
-#   description = "The name of the GCS bucket used for Terraform state"
-# }
+output "terraform_state_bucket_name" {
+  value       = google_storage_bucket.terraform_state.name
+  description = "The name of the GCS bucket used for Terraform state"
+}
 
 output "platform_project_number" {
   value = data.google_project.platform.number
