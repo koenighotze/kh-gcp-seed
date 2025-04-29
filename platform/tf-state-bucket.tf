4e8a3c9a90c9705fcfeccff7bebe766f04aef764
David Schmitz <david.schmitz.privat@gmail.com>
2025-04-29 17:10:20 +0200

    Add bucket for platform state

--- platform/tf-state-bucket.tf
+++ platform/tf-state-bucket.tf
@@ -1,16 +1,16 @@
-# #trivy:ignore:avd-gcp-0066,CKV_GCP_62
-# resource "google_storage_bucket" "terraform_state" {
-#   name     = "${var.project_id}-tf-state"
-#   location = var.location
-#   project  = var.project_id
+#trivy:ignore:avd-gcp-0066,CKV_GCP_62
+resource "google_storage_bucket" "terraform_state" {
+  name     = "${var.project_id}-tf-state"
+  location = var.location
+  project  = var.project_id
 
-#   storage_class = "STANDARD"
+  storage_class = "STANDARD"
 
-#   versioning {
-#     enabled = true
-#   }
+  versioning {
+    enabled = true
+  }
 
-#   public_access_prevention    = "enforced"
-#   uniform_bucket_level_access = true
-# }
+  public_access_prevention    = "enforced"
+  uniform_bucket_level_access = true
+}
 
